d34dffb2e3090d6f6e65494d14397e4139914dff
SecularMonk <54241164+SecularMonk@users.noreply.github.com>
2021-01-27 15:44:32 +0000

    Bug fixes & code cleaning
    Split several trade functions into smaller functions.
    Added array to contain all trading bots info so they don't need to be hard coded. Rewrote functions to iterate over this array rather than hard-coding and repeating function calls.

--- greedy-little-robots/helperfunctions.js
+++ greedy-little-robots/helperfunctions.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+const TradingBot = require('./tradingbots').TradingBot
 
 const con = mysql.createConnection({
     host: "localhost",
@@ -12,31 +13,44 @@ const con = mysql.createConnection({
 });
 
 function getCurrentTimeInSQLFormat() {
-    let date = new Date();   
-    let datetime= date.toISOString().slice(0, 19).replace('T', ' '); 
-    return datetime;
+    try {
+        let date = new Date();   
+        let datetime= date.toISOString().slice(0, 19).replace('T', ' '); 
+        return datetime;
+    } catch(err) {
+        throw err;
+    }
 }
 
 function runAsyncSQLQuery(sql) {
-    return new Promise(function(resolve, reject) {
-        con.query(sql, function(err, rows, fields) {
-            if(err) {
-                return reject(err);
-            }
-            resolve(rows);
+    try {
+        return new Promise(function(resolve, reject) {
+            con.query(sql, function(err, rows, fields) {
+                if(err) {
+                    return reject(err);
+                }
+                resolve(rows);
+            });
         });
-    });
+    } catch(err) {
+        throw err;
+    }
 }
 
   /* Connects to the database to return all the distinct trade items */
 function getListOfTradeItemsFromDatabase() {
-    let sql = `SELECT DISTINCT TradeItem, Class from scrapeddata ORDER BY Class DESC;`
-    return runAsyncSQLQuery(sql);
+    try {
+        let sql = `SELECT DISTINCT TradeItem, Class from scrapeddata ORDER BY Class DESC;`
+        return runAsyncSQLQuery(sql);
+    } catch(err) {
+        throw err;
+    }
 }
 
 /* Looks at each trade item individually to see how volatile it is.
     Volatility is how many times the suggestion has changed in a given time. */
 function calculateVolatility(TradeItem) {
+    try {
         let sql = `SELECT TradeItem,
             SUM(CASE WHEN Suggestion = 'Strong Buy' THEN 1 ELSE 0 END) AS StrongBuy, 
             SUM(CASE WHEN Suggestion = 'Buy' THEN 1 ELSE 0 END) AS Buy,
@@ -52,55 +66,80 @@ function calculateVolatility(TradeItem) {
             ) AS SUBQUERY;`
         
         return runAsyncSQLQuery(sql)
+        } catch(err) {
+            throw err;
+        }
     }
 
 function getRecommendations(TradeItem, numberOfRecommendations=5) {
-    let sql = `SELECT * FROM scrapeddata WHERE TradeItem = '${TradeItem}' ORDER BY DateTime DESC LIMIT ${numberOfRecommendations};`;
-    return runAsyncSQLQuery(sql);
+    try {
+        let sql = `SELECT * FROM scrapeddata WHERE TradeItem = '${TradeItem}' ORDER BY DateTime DESC LIMIT ${numberOfRecommendations};`;
+        return runAsyncSQLQuery(sql);
+    } catch(err) {
+        throw err;
+    }
     }
 
 function createNewTradeHistoryRecord(TradeItem, OpenPrice, Position) {
-    let datetime = getCurrentTimeInSQLFormat();
-    let sql = `INSERT INTO tradehistory (TradeItem, OpenPrice, Position, OpenedAt) VALUES ('${TradeItem}', ${OpenPrice}, '${Position}', '${datetime}');`
-    return runAsyncSQLQuery(sql);
+    try {
+        let datetime = getCurrentTimeInSQLFormat();
+        let sql = `INSERT INTO tradehistory (TradeItem, OpenPrice, Position, OpenedAt) VALUES ('${TradeItem}', ${OpenPrice}, '${Position}', '${datetime}');`
+        return runAsyncSQLQuery(sql);
+    } catch(err) {
+        throw err;
+    }
     }
 
 function updateTradeHistoryRecord(TradeItem, openPrice, ClosePrice, Result) {
-    let datetime = getCurrentTimeInSQLFormat();
-    let selectSQL = `
-    SELECT idTradeHistory FROM tradehistory 
-    WHERE TradeItem = '${TradeItem}' AND OpenPrice = ${openPrice}
-    ORDER BY OpenedAt DESC
-    LIMIT 1`
+    try {
+        let datetime = getCurrentTimeInSQLFormat();
+        let selectSQL = `
+        SELECT idtradeHistory FROM tradehistory 
+        WHERE TradeItem = '${TradeItem}' AND OpenPrice = ${openPrice}
+        ORDER BY OpenedAt DESC
+        LIMIT 1`
 
-    let updateSQL = `
-    UPDATE tradehistory 
-    SET ClosePrice = ${ClosePrice}, ClosedAt = '${datetime}', Result = ${Result}
-    WHERE idTradeHistory =
-    `
+        let updateSQL = `
+        UPDATE tradehistory 
+        SET ClosePrice = ${ClosePrice}, ClosedAt = '${datetime}', Result = ${Result}
+        WHERE idtradeHistory =
+        `
 
-    return runAsyncSQLQuery(selectSQL).then((result) =>
-    helpers.runAsyncSQLQuery(updateSQL + result[0].idtradeHistory))
+        return runAsyncSQLQuery(selectSQL).then((result) =>
+        runAsyncSQLQuery(updateSQL + result[0].idtradeHistory))
+    } catch(err) {
+        throw err;
+    }
 }
-/*
 
-function test() {
-    let selectSQL = `
-    SELECT idtradeHistory FROM tradehistory 
-    WHERE TradeItem = 'GBP/USD' AND OpenPrice = 12
-    ORDER BY OpenedAt DESC
-    LIMIT 1`
+function evaluateThenTrade(tradingBot) {
+    try {
+        getRecommendations(tradingBot._TradeItem).then((recommendations) => 
+        tradingBot.evaluateConditions(recommendations))
+    } catch(err) {
+        throw err;
+    }
+}
 
-    let updateSQL = `
-    UPDATE tradehistory 
-    SET ClosePrice = 13, ClosedAt = '2020-12-06 15:39:33', Result = 1
-    WHERE idTradeHistory =
-    `
-    helpers.runAsyncSQLQuery(selectSQL).then((result) => 
-    helpers.runAsyncSQLQuery(updateSQL + result[0].idtradeHistory)
-    )};
+function allBotsEvaluateThenTrade(tradingBotsArray) {
+    try {
+        for(let i=0; i<tradingBotsArray.length; i++) {
+            evaluateThenTrade(tradingBotsArray[i])
+        }
+    } catch(err) {
+        throw err;
+    }
+}
 
-    */
+function setFundsForTradingBots(tradingBotsArray) {
+    try {
+        for(let i=0; i<tradingBotsArray.length; i++) {
+            tradingBotsArray[i].setMoney();
+        }
+    } catch(err) {
+        throw err;
+    }
+}
 
 module.exports.getCurrentTimeInSQLFormat = getCurrentTimeInSQLFormat
 module.exports.runAsyncSQLQuery = runAsyncSQLQuery
@@ -109,3 +148,6 @@ module.exports.calculateVolatility = calculateVolatility
 module.exports.getRecommendations = getRecommendations
 module.exports.createNewTradeHistoryRecord = createNewTradeHistoryRecord
 module.exports.updateTradeHistoryRecord = updateTradeHistoryRecord
+module.exports.setFundsForTradingBots = setFundsForTradingBots
+module.exports.evaluateThenTrade = evaluateThenTrade
+module.exports.allBotsEvaluateThenTrade = allBotsEvaluateThenTrade
--- greedy-little-robots/tradingbots.js
+++ greedy-little-robots/tradingbots.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql');
-const { promise } = require('selenium-webdriver');
 const helpers = require('./helperfunctions');
 
 /* This is where the magic happens. Describes everything the bot needs to manage it's state,
@@ -13,6 +12,7 @@ class TradingBot {
         this.closePrice = 0;
         this.Money = 0;
         this.StartingMoney = 0;
+        this.lastTradeResult = 0;
     }
 
     set TradeItem(TradeItem) {
@@ -32,27 +32,35 @@ class TradingBot {
         return ((this.Money / this.StartingMoney) * 100) - 100
     }    
 
-   decideToOpenBuyPosition(recommendations) {
-        if(recommendations[4].Suggestion === 'Strong Buy' && recommendations[3].Suggestion === 'Strong Buy' && (recommendations[2].Suggestion) === 'Buy' || recommendations[2].Suggestion === 'Strong Buy') {
-                this.openPosition('Buy', recommendations[4].Price, recommendations)
+    decideToOpenBuyPosition(recommendations) {
+        try{
+            if(recommendations[4].Suggestion === 'Strong Buy' && recommendations[3].Suggestion === 'Strong Buy' && (recommendations[2].Suggestion) === 'Buy' || recommendations[2].Suggestion === 'Strong Buy') {
+            this.openPosition('Buy', recommendations[4].Price)
             }
+        } catch(err) {
+            throw err;
         }
+    }
 
    decideToOpenSellPosition(recommendations) {
-        if(recommendations[4].Suggestion === 'Strong Sell' && recommendations[3].Suggestion === 'Strong Sell' && (recommendations[2].Suggestion) === 'Sell' || recommendations[2].Suggestion === 'Strong Sell') {
-        this.openPosition('Sell', recommendations[4].Price, recommendations)
-            }
+       try {
+            if(recommendations[4].Suggestion === 'Strong Sell' && recommendations[3].Suggestion === 'Strong Sell' && (recommendations[2].Suggestion) === 'Sell' || recommendations[2].Suggestion === 'Strong Sell') {
+                this.openPosition('Sell', recommendations[4].Price)
+                }
+        } catch(err) {
+            throw err;
         }
+    }
 
    decideToCloseBuyPosition(recommendations) {
         if(recommendations[4].Suggestion === 'Strong Sell' || recommendations[3].Suggestion === 'Strong Sell' || (recommendations[3].Suggestion === 'Sell' && recommendations[2].Suggestion === 'Sell')) {
-            this.closePosition(this.positionType, this.amount, this.price, recommendations);
+            this.closePosition(this.positionType, recommendations[4].Price);
         }
    }
 
    decideToCloseSellPosition(recommendations) {
         if(recommendations[4].Suggestion === 'Strong Buy' || recommendations[3].Suggestion === 'Strong Buy' || (recommendations[3].Suggestion === 'Buy' && recommendations[2].Suggestion === 'Buy')) {
-            this.closePosition(this.positionType, this.amount, this.price, recommendations);
+            this.closePosition(this.positionType, recommendations[4].Price);
         }
    }
 
@@ -77,29 +85,31 @@ class TradingBot {
 
    /* Opens a position. Saves the purchase price, position type(buy/sell) and  amount purchased
    to the Bot's state. Changes activePosition to true to show a position is open. */
-   openPosition(positionType, price, recommendations) {
+   openPosition(positionType, price) {
         this.activePosition = true;
         this.positionType = positionType;
-        this.openPrice = parseFloat(recommendations[4].Price);
-        console.log(`${positionType}ing ${this.TradeItem} at ${price}. DateTime: ${helpers.getCurrentTimeInSQLFormat()}`)
+        this.openPrice = parseFloat(price);
+        console.log(`${positionType}ing ${this._TradeItem} at ${this.openPrice}. DateTime: ${helpers.getCurrentTimeInSQLFormat()}`)
         helpers.createNewTradeHistoryRecord(this._TradeItem, this.openPrice, positionType)
    }
 
    /* Closes the current position. Saves the result of the trade to the tradeHistory array as an object,
    which holds the trade item, amount bought/sold, purchase price, sell price, and result of the trade.
    Then clears the data from the state to start fresh. */
-   closePosition(positionType, amount, price, recommendations) {
+   closePosition(positionType, price) {
+        this.activePosition = false;
+        this.closePrice = parseFloat(price);
+
         if(positionType === 'Buy') {
             this.lastTradeResult = this.closePrice - this.openPrice;
         } else if(positionType === 'Sell') {
             this.lastTradeResult = this.openPrice - this.closePrice;
         }
-        
-        console.log(`Closing ${positionType} order on ${this.TradeItem} at ${recommendations[4].Price}. ${amount} units ${positionType==='Buy' ? 'sold' : 'bought'} Result: ${this.lastTradeResult}. Total return: ${this.calculateReturn()}%`)
 
-        this.activePosition = false;
-        this.closePrice = parseFloat(recommendations[4].Price);
         this.Money += this.lastTradeResult;
+        
+        console.log(`Closing ${positionType} order on ${this._TradeItem} at ${this.closePrice}. Result: ${this.lastTradeResult}. Total return: ${this.calculateReturn()}%`)
+
         this.positionType = '';
 
         helpers.updateTradeHistoryRecord(this._TradeItem, this.openPrice, this.closePrice, this.lastTradeResult)
