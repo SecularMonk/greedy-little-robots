1e57eddde86f07fd445930b9311819730f3fee6f
SecularMonk <54241164+SecularMonk@users.noreply.github.com>
2022-10-11 22:33:31 +0100

    Comment out boilerplate code

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react'
-import SingleItemChartContainer from './components/singleItemChart/SingleItemChartContainer';
+import React, { Component } from "react";
+import SingleItemChartContainer from "./components/singleItemChart/SingleItemChartContainer";
+import "./App.css";
 
 class App extends Component {
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <SingleItemChartContainer />
-      </header>
-    </div>
-  );
-  }
+   render() {
+      return (
+         <div className="App">
+            <header className="App-header">
+               {/* <p>
+                  Edit <code>src/App.js</code> and save to reload.
+               </p>
+               <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                  Learn React
+               </a> */}
+               <SingleItemChartContainer />
+            </header>
+         </div>
+      );
+   }
 }
 
 export default App;
