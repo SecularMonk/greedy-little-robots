ebca353afeb8c36351346d61ccb2185e87823187
SecularMonk <54241164+SecularMonk@users.noreply.github.com>
2022-10-11 22:34:39 +0100

    Display additional data on request.

--- client/src/components/singleItemChart/SingleItemChart/SingleItemChart.js
+++ client/src/components/singleItemChart/SingleItemChart/SingleItemChart.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
-import fetch from 'node-fetch';
+import React, { Component } from "react";
+import fetch from "node-fetch";
 /*import './SingleItemChart.css'*/
 
 class SingleItemChart extends Component {
-    constructor(props) {
-        super(props)
-    }
-    
-    render() {
-        return(
-            <div>
-                    <ul>
-                        { 
-                        this.props.tradeItemData ?
-                            this.props.tradeItemData.map(data => {
-                                return <li key={data.idscrapedData}>{data.tradeItem}</li>
-                        }) : <li>No trade data fetched</li>
-                    }
-                    </ul>
-            </div>
-        )
-    }
-};
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return (
+         <div>
+            <ul>
+               {this.props.tradeItemData ? (
+                  this.props.tradeItemData.map((data) => {
+                     return (
+                        <li key={data.idinvesting}>
+                           <h2>{data.tradeItem}</h2>
+                           <h3>{data.summary}</h3>
+                           <h3>{data.summaryLast}</h3>
+                           <h3>{data.price}</h3>
+                           <h4>{data.dateTime}</h4>
+                        </li>
+                     );
+                  })
+               ) : (
+                  <li>No trade data fetched</li>
+               )}
+            </ul>
+         </div>
+      );
+   }
+}
 
 export default SingleItemChart;
