2ccb6d117a6f634b94d8d19e2af2ecc44969beca
SecularMonk <54241164+SecularMonk@users.noreply.github.com>
2022-10-12 21:39:35 +0100

    Refactor rendering into function for better readability

--- client/src/components/singleItemChart/SingleItemChart/SingleItemChart.js
+++ client/src/components/singleItemChart/SingleItemChart/SingleItemChart.js
@@ -7,26 +7,31 @@ class SingleItemChart extends Component {
       super(props);
    }
 
+   renderChart() {
+      try {
+         if (this?.props?.tradeItemData && this.props.tradeItemData?.length !== 0) {
+            return this.props.tradeItemData.map((data) => {
+               return (
+                  <li key={data.idinvesting}>
+                     <h2>{data.tradeItem}</h2>
+                     <h3>{data.summary}</h3>
+                     <h3>{data.summaryLast}</h3>
+                     <h3>{data.price}</h3>
+                     <h4>{data.dateTime}</h4>
+                  </li>
+               );
+            });
+         }
+         return <li>No trade data fetched</li>;
+      } catch (error) {
+         console.log("failed to render SingleItemChart", error);
+      }
+   }
+
    render() {
       return (
          <div>
-            <ul>
-               {this.props.tradeItemData ? (
-                  this.props.tradeItemData.map((data) => {
-                     return (
-                        <li key={data.idinvesting}>
-                           <h2>{data.tradeItem}</h2>
-                           <h3>{data.summary}</h3>
-                           <h3>{data.summaryLast}</h3>
-                           <h3>{data.price}</h3>
-                           <h4>{data.dateTime}</h4>
-                        </li>
-                     );
-                  })
-               ) : (
-                  <li>No trade data fetched</li>
-               )}
-            </ul>
+            <ul>{this.renderChart()}</ul>
          </div>
       );
    }
