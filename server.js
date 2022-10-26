492d0491b1fc24b28d913c318966eebf32090724
SecularMonk <54241164+SecularMonk@users.noreply.github.com>
2022-10-26 20:34:29 +0100

    Add bodyparser. Change get to post in tradeItem API. Add query conversion function.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -14,6 +15,9 @@ const con = mysql.createConnection({
 con.connect(function (err) {
    if (err) throw err;
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 function runAsyncSQLQuery(sql) {
    try {
@@ -39,9 +43,30 @@ function getDistinctTradeItems() {
    }
 }
 
-function getMostRecentEntriesForTradeItem(tradeItem, entries = 5) {
+function getMostRecentEntriesForTradeItem({ tradeItem, body = {}, limit = 5 }) {
    try {
-      let sql = `SELECT idinvesting, price, tradeItem, summary, dateTime from investing WHERE tradeItem = '${tradeItem}' ORDER BY dateTime DESC LIMIT ${entries};`;
+      let sql = `SELECT idinvesting, price, tradeItem, summary, dateTime from investing WHERE tradeItem = "${tradeItem}" `;
+      const sortOptions = body?.sortOptions ?? [];
+      console.log(`sortOptions: ${JSON.stringify(sortOptions)}`);
+      let sortString = "ORDER BY ";
+      if (sortOptions.length > 0) {
+         let sortAdded = false;
+         for (let i = 0, n = body.sortOptions.length; i < n; i++) {
+            console.log(`sortOptions[i]: ${JSON.stringify(sortOptions[i])}`);
+            const value = sortOptions[i]?.value;
+            const order = sortOptions[i]?.order;
+            console.log(`value: ${value}, order: ${order}`);
+            if (!value || !order) continue;
+            if (sortAdded) sortString += ",";
+            sortString += `${value} ${order} `;
+            sortAdded = true;
+         }
+      }
+      if (sortString.length > 9) sql += sortString;
+      if (limit) sql += `LIMIT ${limit} `;
+      // ORDER BY dateTime DESC
+      sql += ";";
+      console.log(`sql: ${sql}`);
       return runAsyncSQLQuery(sql);
    } catch (err) {
       throw err;
@@ -57,19 +82,35 @@ function getTradeItemDetails(tradeItem) {
    }
 }
 
+function getChartControls() {
+   try {
+      return {};
+      return runAsyncSQLQuery(sql);
+   } catch (err) {
+      throw err;
+   }
+}
+
 app.get("/api/allTradeItems", cors(), (req, res) => {
    getDistinctTradeItems().then((items) => res.json(items));
 });
 
-app.get("/api/tradeItems/:tradeItem", cors(), (req, res) => {
+app.post("/api/tradeItems/:tradeItem", cors(), (req, res) => {
    console.log(`req.params: ${JSON.stringify(req?.params)}`);
-   let tradeItem = req.params.tradeItem;
-   getMostRecentEntriesForTradeItem(tradeItem).then((entries) => {
+   console.log(`req.query: ${JSON.stringify(req?.body)}`);
+   const tradeItem = req?.params?.tradeItem;
+   if (!tradeItem) res.json({});
+   const body = req?.body ?? {};
+   getMostRecentEntriesForTradeItem({ tradeItem, body }).then((entries) => {
       res.json(entries);
    });
 });
 
 app.get("/api/chartDetails/", cors(), (req, res) => {
+   getTradeItemDetails("DAX").then((items) => res.json(items));
+});
+
+app.get("/api/controls/chart", cors(), (req, res) => {
    getTradeItemDetails("DAX").then((items) => res.json(items));
 });
 
